7f2e2014f6e4751dfd93ecb5e7be655919e9fcec
Chris Neal <chrisvneal@gmail.com>
2021-02-25 12:11:43 -1000

    Change Leaderboard to LeaderBoard.js

--- src/Components/Pages/Leaderboard.js
+++ src/Components/Pages/Leaderboard.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
 import { Header } from "semantic-ui-react";
 
-<<<<<<< HEAD
-export default class Leaderboard extends Component {
-=======
 export default class LeaderBoard extends Component {
->>>>>>> pages
   render() {
     return (
       <div>
