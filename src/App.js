552f55ce36eca050415d3bbc36802294b9f21858
Chris Neal <chrisvneal@gmail.com>
2021-02-25 08:49:50 -1000

    type: feat
    Change App component to class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./index.scss";
+import React, { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
 }
-
-export default App;
