273ade067bca82c50bbad823bb32cae339228cb3
Chris Neal <chrisvneal@gmail.com>
2021-02-25 10:23:02 -1000

    Add Container to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,29 +4,27 @@ import { Container } from "semantic-ui-react";
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <header id="Header">
         <Container>
-          <header id="Header">
-            <div>
-              <h1>Would You Rather?</h1>
-              <img src="https://via.placeholder.com/100X100" alt="logo" />
-            </div>
-            <nav>
-              <ul>
-                <li>Polls</li>
-                <li>Create A Poll</li>
-                <li>Leaderboard</li>
-                <li>Sign In/Out</li>
-                {/* Make sign in/out dynamic based on whether signed in */}
-              </ul>
-            </nav>
-            <div>
-              <img src="https://via.placeholder.com/50x50" alt="logo" />
-              <p>Username</p>
-            </div>
-          </header>
+          <div>
+            <h1>Would You Rather?</h1>
+            <img src="https://via.placeholder.com/100X100" alt="logo" />
+          </div>
+          <nav>
+            <ul>
+              <li>Polls</li>
+              <li>Create A Poll</li>
+              <li>Leaderboard</li>
+              <li>Sign In/Outs</li>
+              {/* Make sign in/out dynamic based on whether signed in */}
+            </ul>
+          </nav>
+          <div>
+            <img src="https://via.placeholder.com/50x50" alt="logo" />
+            <p>Username</p>
+          </div>
         </Container>
-      </div>
+      </header>
     );
   }
 }
