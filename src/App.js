ebd5e58df69cecf622ff6717ae3e5ed55d38e0be
Chris Neal <chrisvneal@gmail.com>
2021-02-25 15:41:12 -1000

    type: feat
    Wrap App in Grid structure

--- src/App.js
+++ src/App.js
@@ -9,27 +9,40 @@ import UserProfile from "./Components/Pages/UserProfile";
 import Polls from "./Components/Pages/Polls";
 import Login from "./Components/Pages/Login";
 import PollDetails from "./Components/Pages/PollDetails";
+import { Grid } from "semantic-ui-react";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <SiteHeader />
-
-          <main className="ui container">
-            <Switch>
-              <Route path="/" exact component={Polls} />
-              <Route path="/add" component={CreatePoll} />
-              <Route path="/leaderboard" component={LeaderBoard} />
-              <Route path="/user_profile" component={UserProfile} />
-              <Route path="/poll_details" component={PollDetails} />
-              <Route path="/user_login" component={Login} />
-            </Switch>
-            {/* <Route path="/poll_id" component={CreatePoll} /> */}
-          </main>
-
-          <SiteFooter />
+          <Grid>
+            <Grid.Row>
+              <Grid.Column>
+                <SiteHeader />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column columns={16}>
+                <main className="ui container">
+                  <Switch>
+                    <Route path="/" exact component={Polls} />
+                    <Route path="/add" component={CreatePoll} />
+                    <Route path="/leaderboard" component={LeaderBoard} />
+                    <Route path="/user_profile" component={UserProfile} />
+                    <Route path="/poll_details" component={PollDetails} />
+                    <Route path="/user_login" component={Login} />
+                  </Switch>
+                  {/* <Route path="/poll_id" component={CreatePoll} /> */}
+                </main>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <SiteFooter />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </div>
       </Router>
     );
