36556a9d35985bde3c23a1a0d320d973da5cf3b8
Chris Neal <chrisvneal@gmail.com>
2021-02-25 14:08:32 -1000

    Add global styling

--- src/Components/SiteHeader.js
+++ src/Components/SiteHeader.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Container, Header } from "semantic-ui-react";
-// import { Link } from "react-router-dom";
 import SiteNav from "./SiteNav";
 
 export default class SiteHeader extends Component {
