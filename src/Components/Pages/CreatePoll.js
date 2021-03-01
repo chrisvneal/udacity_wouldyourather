e24c7f2b7bb956e6965537528867a8c2e4e67902
Chris Neal <chrisvneal@gmail.com>
2021-02-28 16:26:58 -1000

    type: style
    Refactor form input styles

--- src/Components/Pages/CreatePoll.js
+++ src/Components/Pages/CreatePoll.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Form, Checkbox, Button } from "semantic-ui-react";
+import { Header, Form, Button } from "semantic-ui-react";
 
 export default class CreatePoll extends Component {
   render() {
@@ -11,12 +11,13 @@ export default class CreatePoll extends Component {
           <Form.Field>
             <p>Create two options for your poll, make it fun!</p>
             <Header as="h2" content="Would You Rather...?" />
-            <label>...Option 1</label>
-            <input placeholder="Option 1" />
+            <label>Option 1</label>
+            <input placeholder="...be a superhero" />
           </Form.Field>
+          <span className="or">or</span>
           <Form.Field>
-            <label>...Option 2</label>
-            <input placeholder="Option 2" />
+            <label>Option 2</label>
+            <input placeholder="...be a super villian" />
           </Form.Field>
           <Button type="submit">Create Poll</Button>
         </Form>
