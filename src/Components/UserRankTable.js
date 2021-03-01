45018871a343382979d4f678aa002041e7c663fe
Chris Neal <chrisvneal@gmail.com>
2021-02-28 16:49:25 -1000

    Add UserRankTable component to the Leaderboard

---
+++ src/Components/UserRankTable.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Table } from "semantic-ui-react";
+
+export default class UserRankTable extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h3>{this.props.label}</h3>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+              <Table.HeaderCell>Header</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+              <Table.Cell>Cell</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </React.Fragment>
+    );
+  }
+}
