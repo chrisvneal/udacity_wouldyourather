809d04d13d5878834fedf15776457e95711c0579
Chris Neal <chrisvneal@gmail.com>
2021-02-28 15:05:00 -1000

    type: feat
    Add Form components to Create A Poll page

--- src/Components/Pages/CreatePoll.js
+++ src/Components/Pages/CreatePoll.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Form, Checkbox, Button } from "semantic-ui-react";
 
 export default class CreatePoll extends Component {
   render() {
     return (
       <div>
         <Header as="h2" content="Create A Poll" />
+
+        <Form>
+          <Form.Field>
+            <label>First Name</label>
+            <input placeholder="First Name" />
+          </Form.Field>
+          <Form.Field>
+            <label>Last Name</label>
+            <input placeholder="Last Name" />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label="I agree to the Terms and Conditions" />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
       </div>
     );
   }
