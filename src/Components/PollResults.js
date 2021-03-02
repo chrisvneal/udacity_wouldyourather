dff1f57f5ad15ce9761da4b7680750eacbbb455d
Chris Neal <chrisvneal@gmail.com>
2021-03-01 17:38:19 -1000

    type: feat
    Create PollResults component

---
+++ src/Components/PollResults.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// import { Header } from "semantic-ui-react";
+
+export default class PollResults extends Component {
+  render() {
+    return (
+      <div className="poll-results">
+        <h2>Would you Rather...?</h2>
+      </div>
+    );
+  }
+}
