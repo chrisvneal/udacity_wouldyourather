b18931bfb54b344d62aec308d4f56664247f253f
Chris Neal <chrisvneal@gmail.com>
2021-03-03 17:00:40 -1000

    type: feat
    Add PollCards to PollResults

--- src/Components/PollResults.js
+++ src/Components/PollResults.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PollCard from "./PollCard";
 // import { Header } from "semantic-ui-react";
 
 export default class PollResults extends Component {
@@ -6,7 +7,10 @@ export default class PollResults extends Component {
     return (
       <div className="poll-results">
         <h2 className="center">Would You Rather...?</h2>
-        <div className="poll-results-cards">Poll Results Cards</div>
+        <div className="poll-results-cards">
+          <PollCard />
+          <PollCard />
+        </div>
       </div>
     );
   }
