c8312bdea3bfd3493e765405fb3ab899f21d2ac1
Chris Neal <chrisvneal@gmail.com>
2021-03-03 17:27:18 -1000

    Add classes to PollCard options

--- src/Components/Polls/PollCard.js
+++ src/Components/Polls/PollCard.js
@@ -3,15 +3,13 @@ import { Card } from "semantic-ui-react";
 
 export default function PollCard() {
   return (
-    <React.Fragment>
-      <Card>
-        <Card.Content className="header">Would You Rather...?</Card.Content>
-        <Card.Content className="option">Option</Card.Content>
-        <Card.Content className="footer">
-          <p>author</p>
-          <p>5</p>
-        </Card.Content>
-      </Card>
-    </React.Fragment>
+    <Card>
+      <Card.Content className="header">Would You Rather...?</Card.Content>
+      <Card.Content className="option">Option</Card.Content>
+      <Card.Content className="footer">
+        <p className="poll-card-author">author</p>
+        <p className="poll-card-likes">5</p>
+      </Card.Content>
+    </Card>
   );
 }
