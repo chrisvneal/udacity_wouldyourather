2115a31aa06b4d51e4344fdfc68e5d2ff8e130bc
Chris Neal <chrisvneal@gmail.com>
2021-03-04 13:38:54 -1000

    Get initial data from API into state

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -7,8 +7,8 @@ export const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return (dispatch) => {
-    return new Promise.all([_getUsers, _getQuestions]).then(
-      ({ users, questions }) => {
+    return Promise.all([_getUsers(), _getQuestions()]).then(
+      ([users, questions]) => {
         dispatch(getUsers(users));
         dispatch(getQuestions(questions));
         dispatch(setAuthedUser(AUTHED_ID));
