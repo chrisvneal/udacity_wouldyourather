d0036d8ab844133944d761d80ff981b2dd1bdc0b
Chris Neal <chrisvneal@gmail.com>
2021-03-06 15:03:34 -1000

    Establish separation of questions between tabs

--- src/Components/Polls/PollGrid.js
+++ src/Components/Polls/PollGrid.js
@@ -4,11 +4,44 @@ import { connect } from "react-redux";
 
 class PollGrid extends Component {
   render() {
-    console.log("questionIds object:", this.props.questionIds);
+    const { questions, authedUser, users } = this.props;
+
+    // retrieve and sort [ ids ] for all questions
+    const questionIds = Object.keys(questions).sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp
+    );
+
+    console.log("questionIds object:", questionIds);
+
+    // retrieve and sort [ ids ] for all questions user answered
+    // const answers = users[authedUser]["answers"];
+    // const userAnsweredPollIds = Object.keys(answers).sort(
+    //   (a, b) => answers[b].timestamp - answers[a].timestamp
+    // );
+
+    // retrieve and sort [ ids ] for all questions user didn't answer
+
+    // console.log("users answers: ", userAnsweredPollIds);
+
+    const type = this.props.type;
+
+    let polls;
+    if (type === "answered") {
+      polls = [9698776];
+    }
+
+    if (type === "all") {
+      polls = [7687767, 676696896, 69768];
+    }
+
+    if (type === "unanswered") {
+      polls = [3684343, 4234324, 324234234, 3434234, 242662];
+    }
+
     return (
       <div className="poll-grid">
-        {this.props.questionIds.all.map((id) => (
-          <PollCard key={id} question={id} />
+        {polls.map((poll) => (
+          <PollCard key={poll} question={poll} />
         ))}
       </div>
     );
@@ -16,25 +49,10 @@ class PollGrid extends Component {
 }
 
 function mapStateToProps({ questions, authedUser, users }) {
-  // retrieve and sort [ ids ] for all questions
-  const questionIds = Object.keys(questions).sort(
-    (a, b) => questions[b].timestamp - questions[a].timestamp
-  );
-
-  // retrieve & sort [ ids ] for all questions user answered
-  const answers = users[authedUser]["answers"];
-
-  const userAnsweredPolls = Object.keys(answers).sort(
-    (a, b) => answers[b].timestamp - answers[a].timestamp
-  );
-
   return {
-    questionIds: {
-      answered: userAnsweredPolls,
-      unanswered: ["unanswered"],
-      all: questionIds,
-    },
     authedUser,
+    users,
+    questions,
   };
 }
 
