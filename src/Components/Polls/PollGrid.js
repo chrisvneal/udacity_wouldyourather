5865fb0a20705e24e9c770549597dc2dd34ee81d
Chris Neal <chrisvneal@gmail.com>
2021-03-07 16:15:50 -1000

    Establish conditional statements for showing polls

--- src/Components/Polls/PollGrid.js
+++ src/Components/Polls/PollGrid.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 
 class PollGrid extends Component {
   render() {
-    const { questions, authedUser } = this.props;
+    const { questions, authedUser, users, type } = this.props;
 
     // retrieve and sort [ ids ] for all questions
     // const questionIds = Object.keys(questions).sort(
@@ -15,20 +15,21 @@ class PollGrid extends Component {
       (a, b) => questions[b].timestamp - questions[a].timestamp
     );
     console.clear();
-    console.log("user: ", authedUser);
+    console.log("user: ", users[authedUser].answers);
 
-    let polls = questionIds;
-    // if (type === "answered") {
-    //   polls = [9698776];
-    // }
+    let polls;
+    // let polls = questionIds;
+    if (type === "answered") {
+      polls = [9698776];
+    }
 
-    // if (type === "all") {
-    //   polls = [7687767, 676696896, 69768];
-    // }
+    if (type === "all") {
+      polls = [7687767, 676696896, 69768];
+    }
 
-    // if (type === "unanswered") {
-    //   polls = [3684343, 4234324, 324234234, 3434234, 242662];
-    // }
+    if (type === "unanswered") {
+      polls = [3684343, 4234324, 324234234, 3434234, 242662];
+    }
 
     return (
       <div className="poll-grid">
