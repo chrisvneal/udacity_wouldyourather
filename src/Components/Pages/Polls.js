6a63438ad780307c192ef24892d07aaed015e539
Chris Neal <chrisvneal@gmail.com>
2021-03-11 10:58:56 -1000

    Move functions from PollGrid to Polls component

--- src/Components/Pages/Polls.js
+++ src/Components/Pages/Polls.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+
+import { splitQuestions } from "../../apis/helpers";
 import { Header, Tab, MenuItem, Label } from "semantic-ui-react";
 import PollGrid from "../Polls/PollGrid";
 
@@ -44,7 +46,77 @@ class Polls extends Component {
   ];
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
+    const { questions, authedUser, users, type } = this.props;
+
+    // retrieve and sort [ ids ] for all questions
+    const questIds_sorted = Object.keys(questions).sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp
+    );
+
+    // retrieve and sort [ ids ] for all questions users answered
+    const userAnswers = users[authedUser.id].answers;
+    const userAnswers_sorted = Object.keys(userAnswers).sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp
+    );
+
+    console.clear();
+    // console.log("output: ", userAnswers, userAnswers_sorted);
+    console.log("questions:", questions);
+    console.log("authedUser:", authedUser);
+
+    const sortedPolls = splitQuestions(questIds_sorted, userAnswers_sorted);
+
+    let { all, answered, unanswered } = sortedPolls;
+
+    // const length = questions[all[0]].optionOne.votes.length;
+    // console.log("question length: ", length);
+
+    console.log("sorted polls:", sortedPolls);
+
+    function mapVotesToPoll(pollIds, questions, type) {
+      let pollsWithLikes = pollIds.map((poll) => {
+        // console.log(poll);
+        return {
+          id: poll,
+          author: questions[poll].author,
+          votes: {
+            optionOne: questions[poll].optionOne.votes.length,
+            optionTwo: questions[poll].optionTwo.votes.length,
+            total:
+              questions[poll].optionOne.votes.length +
+              questions[poll].optionTwo.votes.length,
+          },
+          type,
+        };
+      });
+      return pollsWithLikes;
+    }
+
+    all = mapVotesToPoll(all, questions, "all");
+    answered = mapVotesToPoll(answered, questions, "answered");
+    unanswered = mapVotesToPoll(unanswered, questions, "unanswered");
+
+    let polls;
+
+    switch (type) {
+      case "answered":
+        polls = answered;
+        break;
+
+      case "all":
+        polls = all;
+        break;
+
+      case "unanswered":
+        polls = unanswered;
+
+        break;
+      default:
+        polls = ["no questions"];
+    }
+
+    // console.log("polls: ", polls);
     return (
       <div className="polls">
         <Header as="h2" content="Polls" dividing />
--- src/Components/Polls/PollGrid.js
+++ src/Components/Polls/PollGrid.js
@@ -1,80 +1,8 @@
 import React, { Component } from "react";
 import PollCard from "./PollCard";
-import { splitQuestions } from "../../apis/helpers";
 
 export default class PollGrid extends Component {
   render() {
-    const { questions, authedUser, users, type } = this.props;
-
-    // retrieve and sort [ ids ] for all questions
-    const questIds_sorted = Object.keys(questions).sort(
-      (a, b) => questions[b].timestamp - questions[a].timestamp
-    );
-
-    // retrieve and sort [ ids ] for all questions users answered
-    const userAnswers = users[authedUser.id].answers;
-    const userAnswers_sorted = Object.keys(userAnswers).sort(
-      (a, b) => questions[b].timestamp - questions[a].timestamp
-    );
-
-    console.clear();
-    // console.log("output: ", userAnswers, userAnswers_sorted);
-    console.log("questions:", questions);
-    console.log("authedUser:", authedUser);
-
-    const sortedPolls = splitQuestions(questIds_sorted, userAnswers_sorted);
-
-    let { all, answered, unanswered } = sortedPolls;
-
-    // const length = questions[all[0]].optionOne.votes.length;
-    // console.log("question length: ", length);
-
-    console.log("sorted polls:", sortedPolls);
-
-    function mapVotesToPoll(pollIds, questions, type) {
-      let pollsWithLikes = pollIds.map((poll) => {
-        // console.log(poll);
-        return {
-          id: poll,
-          author: questions[poll].author,
-          votes: {
-            optionOne: questions[poll].optionOne.votes.length,
-            optionTwo: questions[poll].optionTwo.votes.length,
-            total:
-              questions[poll].optionOne.votes.length +
-              questions[poll].optionTwo.votes.length,
-          },
-          type,
-        };
-      });
-      return pollsWithLikes;
-    }
-
-    all = mapVotesToPoll(all, questions, "all");
-    answered = mapVotesToPoll(answered, questions, "answered");
-    unanswered = mapVotesToPoll(unanswered, questions, "unanswered");
-
-    let polls;
-
-    switch (type) {
-      case "answered":
-        polls = answered;
-        break;
-
-      case "all":
-        polls = all;
-        break;
-
-      case "unanswered":
-        polls = unanswered;
-
-        break;
-      default:
-        polls = ["no questions"];
-    }
-
-    // console.log("polls: ", polls);
-
     return (
       <div className="poll-grid">
         {polls.map((poll) => (
