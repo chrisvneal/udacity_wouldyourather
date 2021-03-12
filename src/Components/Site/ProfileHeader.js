dd33ff79c58a510ccb452026e0a6502629291c9f
Chris Neal <chrisvneal@gmail.com>
2021-03-11 17:52:29 -1000

    Connect state to ProfileHeader props

--- src/Components/Site/ProfileHeader.js
+++ src/Components/Site/ProfileHeader.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Icon } from "semantic-ui-react";
+import { connect } from "react-redux";
+// import { Icon } from "semantic-ui-react";
 
-export default function ProfileHeader() {
+function ProfileHeader(props) {
   return (
     <div className="profile-header">
       {/* <img src="https://via.placeholder.com/50x50" alt="logo" /> */}
-      <Icon name="user circle" size={"big"} />
-      <p className="username">Username</p>
+      {/* <Icon name="user circle" size={"big"} /> */}
+      <img src={props.avatar} alt="user avatar" />
+      <p className="username">{props.username}</p>
     </div>
   );
 }
+
+function mapStateToProps({ users, authedUser }) {
+  console.log("users", users[authedUser.id], authedUser.id);
+  return {
+    avatar: users[authedUser.id].avatarURL,
+    username: authedUser.id,
+  };
+}
+
+export default connect(mapStateToProps)(ProfileHeader);
