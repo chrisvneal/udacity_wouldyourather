da9ce2c0336db1304b57232eea8e128cd4e314aa
Chris Neal <chrisvneal@gmail.com>
2021-03-12 16:02:06 -1000

    Replace images in profile header

--- src/apis/_DATA.js
+++ src/apis/_DATA.js
@@ -1,8 +1,11 @@
+import tyler_mcginnis from "../images/tyler_mcginnis.png";
+import sarah_edo from "../images/sarah_edo.png";
+import john_doe from "../images/john_doe.png";
 let users = {
   sarahedo: {
     id: "sarahedo",
     name: "Sarah Edo",
-    avatarURL: "/images/wonder_woman.png",
+    avatarURL: sarah_edo,
     answers: {
       "8xf0y6ziyjabvozdd253nd": "optionOne",
       "6ni6ok3ym7mf1p33lnez": "optionTwo",
@@ -14,7 +17,7 @@ let users = {
   tylermcginnis: {
     id: "tylermcginnis",
     name: "Tyler McGinnis",
-    avatarURL: "/images/robin_hood.png",
+    avatarURL: tyler_mcginnis,
     answers: {
       vthrdm985a262al8qx3do: "optionOne",
       xj352vofupe1dqz9emx13r: "optionTwo",
@@ -24,7 +27,7 @@ let users = {
   johndoe: {
     id: "johndoe",
     name: "John Doe",
-    avatarURL: "/images/cap_america.png",
+    avatarURL: john_doe,
     answers: {
       xj352vofupe1dqz9emx13r: "optionOne",
       vthrdm985a262al8qx3do: "optionTwo",
