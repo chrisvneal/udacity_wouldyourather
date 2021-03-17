252043e152d7f7ab342de5901987b9f24fda8055
Chris Neal <chrisvneal@gmail.com>
2021-03-16 18:21:14 -1000

    Add link to poll_details page from poll card

--- src/Components/Polls/PollCard.js
+++ src/Components/Polls/PollCard.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default function PollCard(props) {
   return (
-    <Card>
-      <Card.Content className="header">Would You Rather...?</Card.Content>
-      <Card.Content className="option">{props.question}</Card.Content>
-      <Card.Content className="footer">
-        <p className="poll-card-author">{props.author}</p>
-        <p className="poll-card-likes">{props.likes}</p>
-      </Card.Content>
-    </Card>
+    <Link to="poll_details">
+      <Card>
+        <Card.Content className="header">Would You Rather...?</Card.Content>
+        <Card.Content className="option">{props.question}</Card.Content>
+        <Card.Content className="footer">
+          <p className="poll-card-author">{props.author}</p>
+          <p className="poll-card-likes">{props.likes}</p>
+        </Card.Content>
+      </Card>
+    </Link>
   );
 }
