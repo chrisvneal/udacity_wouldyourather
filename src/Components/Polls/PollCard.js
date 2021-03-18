e7b8b5d0f07714cf2ab0ec49bcf4bcf8e4715804
Chris Neal <chrisvneal@gmail.com>
2021-03-18 00:58:33 -1000

    Remove Link element from Card and place around Content

--- src/Components/Polls/PollCard.js
+++ src/Components/Polls/PollCard.js
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 
 export default function PollCard(props) {
   return (
-    <Link
-      to={{
-        pathname: `${props.id}`,
-      }}>
-      <Card>
+    <Card>
+      <Link
+        to={{
+          pathname: `${props.id}`,
+        }}>
         <Card.Content className="header">Would You Rather...?</Card.Content>
         <Card.Content className="option">{props.question}</Card.Content>
         <Card.Content className="footer">
           <p className="poll-card-author">{props.author}</p>
           <p className="poll-card-likes">{props.likes}</p>
         </Card.Content>
-      </Card>
-    </Link>
+      </Link>
+    </Card>
   );
 }
