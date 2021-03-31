92712778d3c35df4847fc7a7978a766d57b7d5b2
Chris Neal <chrisvneal@gmail.com>
2021-03-31 11:18:56 -1000

    Remove everything returned from saveOption reducer

--- src/reducers/saveOption.js
+++ src/reducers/saveOption.js
@@ -4,29 +4,7 @@ export default function saveOption(state = {}, action) {
   const { authedUser, qid, answer } = action;
   switch (action.type) {
     case SAVE_OPTION:
-      return {
-        ...state,
-        users: {
-          ...state.users,
-          [authedUser]: {
-            ...state.users[authedUser],
-            answers: {
-              ...state.users[authedUser].answers,
-              [qid]: answer,
-            },
-          },
-        },
-        questions: {
-          ...state.questions,
-          [qid]: {
-            ...state.questions[qid],
-            [answer]: {
-              ...state.questions[qid][answer],
-              votes: state.questions[qid][answer].votes.concat([authedUser]),
-            },
-          },
-        },
-      };
+      return {};
     default:
       return state;
   }
