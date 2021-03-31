dc7f345bc424ca1754b20434a9266fcf26da52a9
Chris Neal <chrisvneal@gmail.com>
2021-03-31 12:39:33 -1000

    Add SAVE_OPTION to questions reducer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -9,7 +9,18 @@ export default function questions(state = {}, action) {
         ...action.questions,
       };
     case SAVE_OPTION:
-      return {};
+      return {
+        ...state,
+        [action.qid]: {
+          ...state[action.qid],
+          [action.answer]: {
+            ...state[action.qid][action.answer],
+            votes: state[action.qid][action.answer].votes.concat(
+              action.authedUser
+            ),
+          },
+        },
+      };
     default:
       return state;
   }
