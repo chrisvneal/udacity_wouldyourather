71b8ff5dfe8d818e06dc7ae22afb4d87c58abaa8
Chris Neal <chrisvneal@gmail.com>
2021-04-01 08:58:18 -1000

    Create saveQuestion action creator

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,16 @@
 export const GET_QUESTIONS = "GET_QUESTIONS";
+export const SAVE_QUESTION = "SAVE_QUESTION";
 
 export function getQuestions(questions) {
   return {
     type: GET_QUESTIONS,
     questions,
+  };
+}
+
+export function saveQuestion(question) {
+  return {
+    type: SAVE_QUESTION,
+    question,
   };
 }
