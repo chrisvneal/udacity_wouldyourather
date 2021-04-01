fea03f70392936ec6d60bf3a5a9d4986dacd8103
Chris Neal <chrisvneal@gmail.com>
2021-04-01 12:46:44 -1000

    Add mapStateToProps to PollCard

--- src/Components/Polls/PollCard.js
+++ src/Components/Polls/PollCard.js
@@ -1,22 +1,37 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default function PollCard(props) {
+function PollCard({ id, userAnswered, question, author, likes, selectOption }) {
+  // if id of this card is in the list of users answers, this card should have the style of .asnwered
+  // console.log("card id: ", id);
+
   return (
     // <Card onClick={props.selectOption}>
-    <Card as={"div"} onClick={props.selectOption}>
+    <Card
+      as={"div"}
+      onClick={selectOption}
+      className={userAnswered.includes(id) ? "answered" : null}>
       <Link
         to={{
-          pathname: `${props.id}`,
+          pathname: `${id}`,
         }}>
         <Card.Content className="header">Would You Rather...?</Card.Content>
-        <Card.Content className="option">{props.question}</Card.Content>
+        <Card.Content className="option">{question}</Card.Content>
         <Card.Content className="footer">
-          <p className="poll-card-author">{props.author}</p>
-          <p className="poll-card-likes">{props.likes}</p>
+          <p className="poll-card-author">{author}</p>
+          <p className="poll-card-likes">{likes}</p>
         </Card.Content>
       </Link>
     </Card>
   );
 }
+
+const mapStateToProps = ({ users, authedUser }) => {
+  return {
+    userAnswered: Object.keys(users[authedUser.id].answers),
+  };
+};
+
+export default connect(mapStateToProps)(PollCard);
