a7c676bbf96b2355b7152d550aa4b3b75dcdcc50
Chris Neal <chrisvneal@gmail.com>
2021-04-06 13:22:30 -1000

    Add styling to Error message

--- src/Components/Pages/ErrorPage.js
+++ src/Components/Pages/ErrorPage.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
-    <div>
-      <h1>Error: 404</h1>
+    <div className="error-page">
+      <div className="error">
+        <div className="error-message">
+          <h1>404</h1>
+          <p>Sorry - we can't find that page!</p>
+        </div>
+      </div>
+      <nav>
+        <Link to="/">Return to Login</Link>
+      </nav>
     </div>
   );
 }
