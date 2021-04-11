c5a4520ea70cee23bbd8294668737aa425b0663e
Chris Neal <chrisvneal@gmail.com>
2021-04-10 17:30:24 -1000

    Create function to calculate vote percentage

--- src/Components/Polls/ProgressBar.js
+++ src/Components/Polls/ProgressBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Progress } from "semantic-ui-react";
 
-export default function ProgressBar(props) {
+export default function ProgressBar({ optionVoteCount, pollVoteCount }) {
   //     return <Progress
   //     value={this.getTotalOptionVotes(
   //       questions,
@@ -10,12 +10,21 @@ export default function ProgressBar(props) {
   //     )}
   //     />
 
+  const calculateVotePercentage = (options, votes) => {
+    let calculation = (options / votes) * 100;
+
+    return calculation.toFixed(0);
+  };
+
   return (
-    <Progress className="poll-progress" percent={50} progress>
+    <Progress
+      className="poll-progress"
+      percent={calculateVotePercentage(optionVoteCount, pollVoteCount)}
+      progress>
       <p>
-        <span className="user-info-number">{`${props.optionVoteCount}`}</span>
+        <span className="user-info-number">{`${optionVoteCount}`}</span>
         option votes out of{" "}
-        <span className="user-info-number">{`${props.pollVoteCount}`}</span>
+        <span className="user-info-number">{`${pollVoteCount}`}</span>
         poll votes
       </p>
     </Progress>
